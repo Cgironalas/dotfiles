45e1672e8bf72af8bd2580cc1bcabff862a59612
Carlos Giron <cgironalas@gmail.com>
2020-04-29 16:23:06 -0600

    Improve manjaro install script

--- new_system_installation/manjaro.sh
+++ new_system_installation/manjaro.sh
@@ -2,7 +2,7 @@
 
 cur_dir=$(pwd)
 # Install programs from pacman
-echo "Installing software from pacman"
+echo "\n\n\nInstalling software with pacman"
 sudo pacman -Syu \
   ttf-liberation \
   lutris \
@@ -20,41 +20,69 @@ sudo pacman -Syu \
   xsel \
   xclip
 
-if [ -x "$(command -v yay)" ]; then
-  echo "yay not installed, it will be downloaded and installed"
-  git clone https://our.archlinux.org/yay.git ~/.yay
-  cd ~/.yay
-  makepkg -si
-  cd $cur_dir
-fi
-
-echo "Installing software from yay"
-yay -Syu \
+echo "\n\n\nInstalling software with pamac"
+sudo sed --in-place "s/#EnableAUR/EnableAUR/" "/etc/pamac.conf"
+sudo sed --in-place "s/#CheckAURUpdates/CheckAURUpdates/" "/etc/pamac.conf"
+pamac install \
   discord \
-  slack-desktop-dark \
+  slack-desktop \
   spotify \
   steam \
   telegram-desktop \
   zoom \
   filezilla
 
-echo "Finishing docker installation"
+echo "\n\n\nFinishing docker installation"
   systemctl start docker
   systemctl enable docker
   sudo usermod -aG docker $USER
 echo "Please reboot to be able to use docker"
 
-echo "Finishing snap installation"
+echo "\n\n\nFinishing snap installation"
   sudo systemctl enable —now snapd.socket && \
     echo "installing bitwarden" && \
     snap install bitwarden 
 
-echo "Installing vimplug"
+echo "\n\n\nInstalling vimplug"
 cd ~ && \ 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cur_dir=$(pwd)
 #manjaro
 
-#xfce
-/usr/bin/setxkbmap -option "ctrl:nocaps"
+mkdir ~/repos
+mkdir ~/sandbox
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf && \
+  cd ~/.asdf && /
+  git checkout "$(git describe --abbrev=0 --tags)"
+
+git clone https://github.com/cgironalas/dotfiles.git ~/dotfiles && \
+  cd ~/dotfiles && \
+  make dotfiles
+
+source ~/.asdf/asdf.sh && \
+      asdf plugin-add python && \
+      asdf install python 2.7.15 && \
+      asdf install python 3.7.4 && \
+      asdf install python 3.8.2 && \
+      asdf global python 3.8.2 && \
+      pip install -U \
+            pip \
+            pynvim \
+            jedi-language-server \
+            black \
+            isort \
+            neovim-remote \
+            pylint \
+            toml-sort && \
+      asdf reshim python
+
+source ~/.asdf/asdf.sh && \
+      asdf plugin-add nodejs && \
+      asdf plugin-add yarn && \
+      bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring && \
+      asdf install nodejs 12.9.1 && \
+      asdf install yarn 1.17.3 && \
+      asdf global nodejs 12.9.1 && \
+      asdf global yarn 1.17.3
